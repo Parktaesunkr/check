55784191008d1e4fafd20ab248c321c2f5c872bf
spcsm5452 <spcsm5452@naver.com>
2023-08-29 15:31:26 +0900

    변경 사항

--- src/main/java/com/spr/neterpark/repository/ReplyRepository.java
+++ src/main/java/com/spr/neterpark/repository/ReplyRepository.java
@@ -1,7 +1,7 @@
 package com.spr.neterpark.repository;
 
-import com.spr.neterpark.entity.Reply;
+import com.spr.neterpark.entity.Replpy;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReplyRepository extends JpaRepository<Reply, Integer> {
+public interface ReplyRepository extends JpaRepository<Replpy, Integer> {
 }
